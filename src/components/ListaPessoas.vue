155f5129c4887e8da665dd7729356c076f8d2319
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-22 21:19:46 -0400

    criando componentes lista

---
+++ src/components/ListaPessoas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{titulo}}</h3>
+    <input type="text" placeholder="Nome da Pessoa" v-model="item" />
+    <button type="button" @click="adicionarItem()">Adicionar</button>
+    <br />
+    <ul>
+      <li v-for="(item, index) in itens" :key="index">{{ item }}</li>
+    </ul>
+    <button type="button" @click="removerItem">Remover</button>
+  </div>
+</template>
+
+<script>
+import ListaMixin from "@/mixins/ListaMixin";
+import RemoverItemListaMixin from "@/mixins/RemoverItemListaMixin";
+export default {
+  name: "ListaPessoas",
+  data : () =>({
+    titulo: 'Lista Pessoas'
+  }),
+  mixins: [ListaMixin, RemoverItemListaMixin],
+};
+</script>
